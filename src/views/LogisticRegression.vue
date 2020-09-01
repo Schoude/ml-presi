c3bed5c87357654fd82c4247a63acae5ed3408e9
Marc <marcbaque1311@gmail.com>
2020-09-02 01:05:09 +0200

    [ ADD ] model compilation and train function

--- src/views/LogisticRegression.vue
+++ src/views/LogisticRegression.vue
@@ -3,35 +3,83 @@
   .header
     h3 Prediction of a binary outcome
     img.lin-vs-log(src="../assets/lin-vs-log-reg.jpg")
+  .content
+    button(@click="trainModel") Train Model
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, onBeforeUnmount } from "vue";
 import {
   loadCSVData,
   createDatasets,
 } from "../compositions/logisticRegression";
 
 import { KeyableObject } from "../typings/basic.d";
+import * as tf from "@tensorflow/tfjs";
 
 export default defineComponent({
   name: "LogisticRegression",
   async setup() {
+    const logModel = tf.sequential();
+    onBeforeUnmount(() => {
+      logModel.dispose();
+    });
     const data = (await loadCSVData()) as KeyableObject;
-    console.log("raw data", data);
-    const [trainData, validationDatal] = createDatasets(
+
+    const [trainData, validationData] = createDatasets(
       data,
-      ["size", "rooms"],
+      // ["size", "rooms"],
+      ["size"],
       "sold",
       0.1,
       32
     );
 
-    // log datasets
-    // const trainVals = trainDs.take(10);
-    // // xs: shape[32, 2] -> 32 = batchSize; 2 = number of features
-    // // ys: shape[32, 2] -> 32 = batchSize; 2 = binary oneHot coding
-    // trainVals.forEachAsync((v) => console.log(v));
+    /**
+     * Utilizes Softmax Regression to adjust the weights as activation models.
+     * Suitable for a logistic regression model.
+     *
+     * optimizer = tf.train.adam()
+     * loss fn = 'binaryCrossentropy'
+     */
+    async function trainLogisticRegression(
+      featureCount: number
+      // trainData: any,
+      // validationData: any
+    ) {
+      // add layers to model
+      logModel.add(
+        tf.layers.dense({
+          // because of "oneHot" encoding
+          units: 2,
+          activation: "softmax",
+          inputShape: [featureCount],
+        })
+      );
+
+      // compile the mode
+      logModel.compile({
+        optimizer: tf.train.adam(0.001),
+        loss: "binaryCrossentropy",
+        metrics: ["accuracy"],
+      });
+    }
+
+    /**
+     * Train the model with then training data and the validation data.
+     */
+    async function trainModel() {
+      await logModel.fitDataset(trainData, {
+        epochs: 10,
+        validationData: validationData,
+      });
+      console.log("done training");
+      console.log(tf.memory().numTensors);
+    }
+
+    await trainLogisticRegression(1);
+    // logModel.summary();
+    return { trainModel };
   },
 });
 </script>
